8149170e758a3996b29c9a7340442da70b39e5ae
Miklos Koren <miklos.koren@gmail.com>
2021-03-25 17:00:52 +0100

    Add svelte-md app

---
+++ src/App.svelte
@@ -0,0 +1,39 @@
+<script>
+	export let name;
+	import Readme from './26460.md';
+</script>
+
+<main>
+	<Readme />
+</main>
+
+<style>
+	main {
+		font-family: 'Open Sans', sans-serif;
+		text-align: left;
+		padding: 1em;
+		max-width: 420px;
+		margin: 0 auto;
+	}
+
+	:global(h1) {
+		color: #9b2701;
+		font-size: 2em;
+		font-weight: 200;
+	}
+
+	:global(annotate) {
+		vertical-align: super;  
+		display: inline-block;
+		color: #5003a8;
+		font-family: 'Caveat', cursive;
+		font-size: 1.2em;
+		font-weight: 200;
+	}
+
+	@media (min-width: 720px) {
+		main {
+			max-width: 540px;
+		}
+	}
+</style>
