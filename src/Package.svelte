ace800f7855b400b6f69f6f41b1729e2a1f68178
Miklos Koren <miklos.koren@gmail.com>
2021-06-02 18:58:51 +0200

    Give another promise to parse markdown meta

--- src/Package.svelte
+++ src/Package.svelte
@@ -2,37 +2,33 @@
 	export let params = {};
 	import marked from 'marked';
 	import metadataParser from 'markdown-yaml-metadata-parser';
-import { log } from 'node:console';
+	import { log } from 'node:console';
 
     let ms_number = params.ms_number;
 	let journal = params.journal;
 	async function fetchPackage() {
 		let res = await fetch(journal + '/' + ms_number + '.md')
 		let markdown = await res.text();
+
 		return markdown;
 	}
-	const promise = fetchPackage();
-
-	export let markdown;
-	export let meta;
-	export let obj;
+	const promise = fetchPackage()
+		.then(text => { 
+			console.log('----', text); 
+			return text; 
+		})
+		.then(text => metadataParser(text));
 </script>
 
 <main>
 <div class="content">
-	{#await promise}
+{#await promise}
 	<p>Loading MS number {ms_number}...	</p>
-{:then text}
-<script>
-	console.log(typeof(text));
-	$: obj = metadataParser(text);
-	$: meta = obj.metadata;
-	$: markdown = obj.content;
-</script>
-	<h1><a href="{meta.package_doi}">{meta.title}</a></h1>
-	<h2>Authors: {meta.authors}</h2>
-	<h2>in <a href="{meta.article_doi}">{meta.journal}</a></h2>
-	{@html marked(markdown)}
+{:then obj}
+	<h1><a href="{obj.metadata.package_doi}">{obj.metadata.title}</a></h1>
+	<h2>Authors: {obj.metadata.authors}</h2>
+	<h2>in <a href="{obj.metadata.article_doi}">{obj.metadata.journal}</a></h2>
+	{@html marked(obj.content)}
 {:catch error}
 	<p style="color: red">{error.message}</p>
 {/await}	
