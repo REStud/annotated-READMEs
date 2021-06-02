3cfc2e6b106d688b82a5ff7007d60f815cee333d
Miklos Koren <miklos.koren@gmail.com>
2021-06-02 15:08:51 +0200

    Build site with Svelte

--- src/Package.svelte
+++ src/Package.svelte
@@ -2,23 +2,37 @@
 	export let params = {};
 	import marked from 'marked';
 	import metadataParser from 'markdown-yaml-metadata-parser';
+import { log } from 'node:console';
 
     let ms_number = params.ms_number;
 	let journal = params.journal;
 	async function fetchPackage() {
 		let res = await fetch(journal + '/' + ms_number + '.md')
-		let markdown = await metadataParser(res.text());
+		let markdown = await res.text();
 		return markdown;
 	}
 	const promise = fetchPackage();
+
+	export let markdown;
+	export let meta;
+	export let obj;
 </script>
 
 <main>
 <div class="content">
 	{#await promise}
 	<p>Loading MS number {ms_number}...	</p>
-{:then markdown}
-	{@html marked(markdown.content)}
+{:then text}
+<script>
+	console.log(typeof(text));
+	$: obj = metadataParser(text);
+	$: meta = obj.metadata;
+	$: markdown = obj.content;
+</script>
+	<h1><a href="{meta.package_doi}">{meta.title}</a></h1>
+	<h2>Authors: {meta.authors}</h2>
+	<h2>in <a href="{meta.article_doi}">{meta.journal}</a></h2>
+	{@html marked(markdown)}
 {:catch error}
 	<p style="color: red">{error.message}</p>
 {/await}	
