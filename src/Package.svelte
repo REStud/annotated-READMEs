3525f6cd6cfc76412a1de2856c2be163084eb059
Miklos Koren <miklos.koren@gmail.com>
2021-06-02 14:40:02 +0200

    Pass markdown through metadataParser (#4, #7)

--- src/Package.svelte
+++ src/Package.svelte
@@ -1,12 +1,13 @@
 <script>
 	export let params = {};
 	import marked from 'marked';
+	import metadataParser from 'markdown-yaml-metadata-parser';
 
     let ms_number = params.ms_number;
 	let journal = params.journal;
 	async function fetchPackage() {
 		let res = await fetch(journal + '/' + ms_number + '.md')
-		let markdown = await res.text();
+		let markdown = await metadataParser(res.text());
 		return markdown;
 	}
 	const promise = fetchPackage();
@@ -17,7 +18,7 @@
 	{#await promise}
 	<p>Loading MS number {ms_number}...	</p>
 {:then markdown}
-	{@html marked(markdown)}
+	{@html marked(markdown.content)}
 {:catch error}
 	<p style="color: red">{error.message}</p>
 {/await}	
